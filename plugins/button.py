67bcd58e42d9bec1ae9671bcc6181a0ea5509cf5
aniblakulgu <142817962+aniblakulgu@users.noreply.github.com>
2024-01-29 08:23:58 +0500

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -87,10 +87,10 @@ def fsub_button(client, message):
     if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="Play Market | Vzlom Oʻyinlar | Ilovalar", url=client.invitelink),
+                InlineKeyboardButton(text="Naruto - Uzbek Tilida", url=client.invitelink),
             ],
             [   
-                InlineKeyboardButton(text="TARJIMA ANIMELAR | UZGOANIME", url=client.invitelink2)
+                InlineKeyboardButton(text="Animelar - Uzbek Tilida", url=client.invitelink2)
             ]
         ]
         try:
