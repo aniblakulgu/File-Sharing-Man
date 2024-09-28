c6a5cb3a73f7ba0b40d9919a06fb6007046c223e
aniblakulgu <142817962+aniblakulgu@users.noreply.github.com>
2024-09-28 10:37:08 +0500

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -93,7 +93,7 @@ def fsub_button(client, message):
                 InlineKeyboardButton(text="Ongoing Animelar | Yoz", url=client.invitelink2),
             ],
             [
-                InlineKeyboardButton(text="Naruto - Official", url=client.invitelink3),
+                InlineKeyboardButton(text="Naruto - Official Uzb", url=client.invitelink3),
             ],
         ]
         try:
