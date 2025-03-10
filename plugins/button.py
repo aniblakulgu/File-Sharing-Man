99f6a34d700d978f645ff287b2aa65374b5da884
aniblakulgu <142817962+aniblakulgu@users.noreply.github.com>
2025-03-10 23:08:04 +0500

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -90,10 +90,10 @@ def fsub_button(client, message):
                 InlineKeyboardButton(text="Yolg'izlikda daraja ko'tarish", url=client.invitelink),
             ],
             [
-                InlineKeyboardButton(text="Animelar - Uzbek Tilida", url=client.invitelink2),
+                InlineKeyboardButton(text="Sirlar Hukmdori - Uzbekcha", url=client.invitelink2),
             ],
             [
-                InlineKeyboardButton(text="X-Dubbing - Dublaj Uzb", url=client.invitelink3),
+                InlineKeyboardButton(text="Ongoing Mangalar - Uzb", url=client.invitelink3),
             ],
         ]
         try:
